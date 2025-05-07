cf9550b4aed083c935f5c5d454e9dda00aba784d
Kevin K <76533954+KevinisATerribleCoder@users.noreply.github.com>
2025-05-07 02:09:10 +0100

    Update PartTimeStaff.java

--- PartTimeStaff.java
+++ PartTimeStaff.java
@@ -1,84 +1,76 @@
-
 /**
- * The main class.
+ * description here.
  *
  * @author (Kevin)
- * @version (1.2)
+ * @version (1.3)
  */
-public class StaffHire
-{
-    public int vacancyNumber;
-    public String designation;
-    public String jobType;
-    public String staffName;
-    public String joinDate;
-    public String qualification;
-    public String appointedBy;
-    public boolean joined;
 
-    public StaffHire(int vacancyNumber, String designation, String jobType, String staffName, String joinDate, String qualification, String appointedBy, boolean joined) {
-        this.vacancyNumber = vacancyNumber;
-        this.designation = designation;
-        this.jobType = jobType;
-        this.staffName = staffName;
-        this.joinDate = joinDate;
-        this.qualification = qualification;
-        this.appointedBy = appointedBy;
-        this.joined = joined;
-    }
+ public class PartTimeStaff extends StaffHire
+ {
+     public int workingHour;
+     public double wagesPerHour;
+     public String shifts;
+     public boolean terminated;
+ 
+     public PartTimeStaff(int vacancyNumber, String designation, String jobType, String staffName, String joinDate, String qualification, String appointedBy, boolean joined, int workingHour, double wagesPerHour, String shifts) {
+         super(vacancyNumber, designation, jobType, staffName, joinDate, qualification, appointedBy, joined);
+         this.workingHour = workingHour;
+         this.wagesPerHour = wagesPerHour;
+         this.shifts = shifts;
+         this.terminated = false;
+     }
+ 
+     public void setWorkingHour(int workingHour)
+     {
+         this.workingHour = workingHour;
+     }
+ 
+     public void setWagesPerHour(double wagesPerHour)
+     {
+         this.wagesPerHour = wagesPerHour;
+     }
+ 
+     public void setShifts(String shifts)
+     {
+         if (isJoined())
+         {
+             this.shifts = shifts;
+         }
+     }
 
-    public void setVacancyNumber(int vacancyNumber)
-    {
-        this.vacancyNumber = vacancyNumber;
-    }
+     public void display()
+     {
+         System.out.println("Vacancy Number: " + vacancyNumber);
+         System.out.println("Designation: " + designation);
+         System.out.println("Job type: " + jobType);
+         System.out.println("Staff Name: " + staffName);
+         System.out.println("Joining Date: " + joinDate);
+         System.out.println("Qualification: " + qualification);
+         System.out.println("Appointed by: " + appointedBy);
+         System.out.println("Has person joined?: " + joined);
+         if (isJoined())
+         {
+             System.out.println("Shifts:" +shifts);
+             System.out.println("Working Hours: " + workingHour);
+             System.out.println("Wages per hour: " +wagesPerHour);
+         }
+     }
 
-    public void setDesignation(String designation)
-    {
-        this.designation = designation;
-    }
-
-    public void setJobType(String jobType)
-    {
-        this.jobType = jobType;
-    }
-
-    public void setStaffName(String staffName)
-    {
-        this.staffName = staffName;
-    }
-
-    public void setJoinDate(String joinDate)
-    {
-        this.joinDate = joinDate;
-    }
-
-    public void setQualification(String qualification)
-    {
-        this.qualification = qualification;
-    }
-
-    public void setAppointedBy(String appointedBy)
-    {
-        this.appointedBy = appointedBy;
-    }
-
-    public boolean isJoined() {
-        return joined;
-    }
-
-    public void setJoined(boolean joined)
-    {
-        this.joined = joined;
-    }
-
-    public void display() {
-        System.out.println("Vacancy Number: " + vacancyNumber);
-        System.out.println("Designation: " + designation);
-        System.out.println("Job type: " + jobType);
-        System.out.println("Staff Name: " + staffName);
-        System.out.println("Joining Date: " + joinDate);
-        System.out.println("Qualification: " + qualification);
-        System.out.println("Appointed by: " + appointedBy);
-        System.out.println("Has person joined?: " + joined);
-    }
-}
+     public void terminate()
+     {
+     if (terminated)
+     {
+         System.out.println("Staff is already terminated.");
+     }
+     else
+     {
+         setStaffName("");
+         setJoinDate("");
+         setQualification("");
+         setAppointedBy("");
+         setJoined(false);
+         terminated = true;
+     }
+     }
+ 
+ }
